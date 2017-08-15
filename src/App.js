c306dc000f498026876d1856cd1c2484f0ff8237
Carlie Ostrom <rlietrom@gmail.com>
2017-08-15 13:41:30 -0700

    b

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import './App.css';
-import {Button, Form, Input, List, Header, Grid, Segment, Radio, Checkbox} from 'semantic-ui-react'
+import {Button, Form, Input, List, Header, Grid, Segment, Radio, Checkbox, Icon, Popup, Statistic} from 'semantic-ui-react'
 import BarChart from './barchart'
 import ReChart from './reChart'
 import Settings from './settings'
@@ -31,39 +31,35 @@ class App extends Component {
   componentWillMount() {
     axios({
       method: 'GET',
-      // url: 'http://localhost:3000/getfutures'
-      url: 'https://merch-visualizer.herokuapp.com/getfutures'
+      url: 'http://localhost:3000/getfutures'
+      // url: 'https://merch-visualizer.herokuapp.com/getfutures'
     })
     .then(resp => {
       if(resp.data.success) {
         var lists = resp.data.lists
         var profitBefore;
+        var profitMax = 0
+        var profitMaxMonth;
         var newArr = lists.map((future, index) => {
-          console.log('PROFITBEFORE, before', profitBefore)
           var physical;
           var months = index + 1
           if(this.state.quote) {physical = this.state.quote}
           else {physical = this.state.spot}
           var actualProfit = future.last*0.01 - physical - (this.state.fixedCost + this.state.costs*months)
+          var label =  future.month_year.substring(0, 3)
           var margProfit;
           if(index === 0) {margProfit = actualProfit}
           else {margProfit = actualProfit - profitBefore}
           profitBefore = actualProfit
-          console.log('margProfit', margProfit)
           var minMargin = this.state.minMargin
-
           future.minMargin = minMargin
           future.profit = actualProfit
-          future.margProfit = margProfit
-          future.last = future.last*.01
-          console.log('PROFITBEFORE, after', profitBefore)
-          // var label = future.month_year.map((month) => {
-          //   return month.substring(0, 3)
-          // })
+          future.marginal_profit = margProfit
+          future.month_year = label
           return future
         })
-
-        this.setState({data: newArr})
+        console.log('profit max', profitMax, profitMaxMonth)
+        this.setState({data: newArr, profitMax: profitMax, profitMaxMonth: profitMaxMonth})
       } else {
         console.log('not successful /getfutures')
       }
@@ -71,7 +67,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log('fut, prof, marg', this.state.fut, this.state.prof, this.state.marg)
+
   }
 
   changeCosts(e) {
@@ -103,7 +99,7 @@ class App extends Component {
       if(this.state.quote) {physical = this.state.quote}
       else {physical = this.state.spot}
       future["profit"] = future["last"] - physical - (this.state.fixedCost + this.state.costs*months)
-      future["margProfit"] = (future["profit"])/months
+      future["marginal_profit"] = (future["profit"])/months
       future["minMargin"] = this.state.minMargin
       return future
     })
@@ -119,25 +115,25 @@ class App extends Component {
       console.log('TRUEEE')
       var mappedData = this.state.data.map((month) => {
         month["profit"] = month["profit"]*41000
-        month["margProfit"] = month["margProfit"]*41000
+        month["marginal_profit"] = month["marginal_profit"]*41000
         month["last"] = month["last"]*41000
         month["minMargin"] = month["minMargin"]*41000
         return month
       })
-    console.log('MAPPED DATA', mappedData)
-    this.setState({data: mappedData, dollarsPerPound: false})
-  } else {
-    console.log('TRUEEE')
-    var unmappedData = this.state.data.map((month) => {
-      month["profit"] = month["profit"]/41000
-      month["margProfit"] = month["margProfit"]/41000
-      month["last"] = month["last"]/41000
-      month["minMargin"] = month["minMargin"]/41000
-      return month
-    })
-  console.log('UNMAPPED DATA', unmappedData)
-  this.setState({data: unmappedData, dollarsPerPound: true})
-  }
+      console.log('MAPPED DATA', mappedData)
+      this.setState({data: mappedData, dollarsPerPound: false})
+    } else {
+      console.log('TRUEEE')
+      var unmappedData = this.state.data.map((month) => {
+        month["profit"] = month["profit"]/41000
+        month["marginal_profit"] = month["marginal_profit"]/41000
+        month["last"] = month["last"]/41000
+        month["minMargin"] = month["minMargin"]/41000
+        return month
+      })
+      console.log('UNMAPPED DATA', unmappedData)
+      this.setState({data: unmappedData, dollarsPerPound: true})
+    }
   }
 
   settingsContainer() {
@@ -145,85 +141,99 @@ class App extends Component {
       <Grid centered columns={2} divided padded={true}>
         <Grid.Row columns={1}>
           <Grid.Column>
-          <Segment compact={false} textAlign='center'>
-            <Header.Content as='h1'>Cash & Carry Visualizer: Non-fat Dry Milk
-              <Header.Subheader as='h4'>A tool for quickly examining a commodity's futures vs. spot price arbitrage opportunity. Prices update with CME public information.</Header.Subheader>
-            </Header.Content>
-
-          </Segment>
+            <Segment compact={false} textAlign='center'>
+              <Header.Content as='h1'>Cash & Carry Visualizer: Non-fat Dry Milk
+                <Header.Subheader as='h4'>A tool for quickly examining a commodity's futures vs. spot price arbitrage opportunity.</Header.Subheader>
+              </Header.Content>
+            </Segment>
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row
-          >
-          <Grid.Column width={3} >
-            <Segment textAlign='right'>
-              <Header.Content as='h3'>Settings</Header.Content>
-              <Form>
-                <Form.Field>
-                  <label>spot/quote</label>
-                  <Form.Input
-                    textAlign='right'
-                    placeholder={this.state.spot}
-                    onChange={(e) => this.changeQuote(e)}/>
-                  <label>minimum profit margin</label>
-                  <Form.Input
-                    placeholder='.08'
-                    onChange={(e) => this.changeMinMargin(e)}/>
-                  <label>fixed costs</label>
-                  <Form.Input
-                    placeholder='.01'
-                    onChange={(e) => this.changeFixedCost(e)}/>
-                  <label>cost per month</label>
-                  <Form.Input
-                    placeholder='.003'
-                    onChange={(e) => this.changeCosts(e)}/>
-                  <Form.Button
-                    basic
-                    type='submit'
-                    onClick={(e) => this.calculateProfit(e)}>Apply</Form.Button>
-                  </Form.Field>
-                </Form>
-              </Segment>
-              <Segment textAlign='right'>Calculations based on 41,000 lb load sizes</Segment>
-                  </Grid.Column>
-                  <Grid.Column width={12} stretched={true}>
-                    <Segment>
-                      <Form>
-                        <Form.Group inline>
-                          <label>View</label>
-                          <Form.Checkbox label='Futures' value='fut' checked={this.state.fut} onChange={() => this.setState({fut: !this.state.fut})} />
-                          <Form.Checkbox label='Profit' value='prof' checked={this.state.prof} onChange={() => this.setState({prof: !this.state.prof})}/>
-                          <Form.Checkbox label='Marginal Profit' value='marg' checked={this.state.marg} onChange={() => this.setState({marg:!this.state.marg})} />
-                          <Form.Checkbox
-                            slider
-                            defaultChecked
-                            label='$/lb'
-                            onClick={(e) => this.changeUnits(e)} >
-                          </Form.Checkbox>
-                        </Form.Group>
-                      </Form>
-                      {this.state.data.length > 0 ?
-                      <ReChart
-                        dollarsPerPound={this.state.dollarsPerPound}
-                        fut={this.state.fut}
-                        prof={this.state.prof}
-                        marg={this.state.marg}
-                        data={this.state.data}
-                      /> : <div> {this.state.data} </div>}
-                    </Segment>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            )
-          }
+        <Grid.Row>
+            <Grid.Column width={3} >
+              <Segment textAlign='right'>
+                <Header.Content as='h3'>Settings</Header.Content>
+                <Form>
+                  <Form.Field>
+                    <label>spot/quote</label>
+                    <Form.Input
+                      textAlign='right'
+                      placeholder={this.state.spot}
+                      onChange={(e) => this.changeQuote(e)}/>
+                      <label>minimum profit margin</label>
+                      <Form.Input
+                        placeholder='.08'
+                        onChange={(e) => this.changeMinMargin(e)}/>
+                        <label>fixed costs</label>
+                        <Form.Input
+                          placeholder='.01'
+                          onChange={(e) => this.changeFixedCost(e)}/>
+                          <label>cost per month</label>
+                          <Form.Input
+                            placeholder='.003'
+                            onChange={(e) => this.changeCosts(e)}/>
+                              <Form.Button
+                                basic
+                                type='submit'
+                                onClick={(e) => this.calculateProfit(e)}>Apply</Form.Button>
+                            </Form.Field>
+                          </Form>
+                          <Popup
+                            trigger={<Button circular={true} size='medium' basic compact={true}>?</Button>}
+                            content={<List bulleted>
+                              <List.Item>Calculations are based on a 41,000 lb load size</List.Item>
+                              <List.Item>Placeholders represent fixed (paperwork, 'in & out' fees for warehouses) and variable costs (monthly warehouse fees) from my personal experience but it's up to each user to do their own research</List.Item>
+                              <List.Item>The daily spot price is a good place to start, but I suggest calling suppliers to get real quotes on product</List.Item></List>}
+                            basic
+                          />
+                        </Segment>
+                      </Grid.Column>
+                      <Grid.Column width={10} stretched={true}>
+                        <Segment>
+                          <Form>
+                            <Form.Group inline>
+                              <label>View</label>
+                              <Form.Checkbox label='Futures' value='fut' checked={this.state.fut} onChange={() => this.setState({fut: !this.state.fut})} />
+                              <Form.Checkbox label='Profit' value='prof' checked={this.state.prof} onChange={() => this.setState({prof: !this.state.prof})}/>
+                              <Form.Checkbox label='Marginal Profit' value='marg' checked={this.state.marg} onChange={() => this.setState({marg:!this.state.marg})} />
+                              <Form.Checkbox
+                                slider
+                                defaultChecked
+                                label='$/lb'
+                                onClick={(e) => this.changeUnits(e)} >
+                              </Form.Checkbox>
+                              <Popup
+                                trigger={<Button circular={true} size='medium' basic compact={true}>?</Button>}
+                                content={<List bulleted>
+                                  <List.Item>Prices update with each page refresh and reflect futures contract prices from the CME</List.Item>
+                                  <List.Item>When a month's profit is larger than your required margin, its bar will turn bright green</List.Item>
+                                </List>}
+                              />
+                            </Form.Group>
+                          </Form>
+                          {this.state.data.length > 0 ?
+                            <ReChart
+                              dollarsPerPound={this.state.dollarsPerPound}
+                              fut={this.state.fut}
+                              prof={this.state.prof}
+                              marg={this.state.marg}
+                              data={this.state.data}
+                            /> : <div> {this.state.data} </div>}
+                          </Segment>
+                        </Grid.Column>
+                        {/* <Grid.Column width={3}>
+                        </Grid.Column> */}
+                      </Grid.Row>
+                    </Grid>
+                  )
+                }
 
-          render() {
-            console.log('this.state.radio', this.state.radio)
-            return (
-            <div className="App">
-              {this.settingsContainer()}
-            </div>
-          )
-          }
-        }
-        export default App;
+                render() {
+                  console.log('this.state.radio', this.state.radio)
+                  return (
+                    <div className="App">
+                      {this.settingsContainer()}
+                    </div>
+                  )
+                }
+              }
+              export default App;
--- src/reChart.js
+++ src/reChart.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BarChart, Bar, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Cell} from 'recharts'
+import {ResponsiveContainer, BarChart, Bar, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Cell, Statistic} from 'recharts'
 
 
 class ReChart extends Component{
@@ -9,8 +9,13 @@ class ReChart extends Component{
     }
   }
 
-	render () {
+  render () {
     var data = this.props.data
+    data.map((month) => {
+      month.last = parseFloat(month.last.toFixed(5))
+      month.marginal_profit = parseFloat(month.marginal_profit.toFixed(5))
+      month.profit = parseFloat(month.profit.toFixed(5))
+    })
     console.log('DATA in RECHART', data)
     var value = this.props.value
     var fut = this.props.fut
@@ -19,32 +24,32 @@ class ReChart extends Component{
     var dollarsPerPound = this.props.dollarsPerPound
     return (
       <div>
-    	<BarChart
-        width={700}
-        height={450}
-        margin={{top: 10, right: 10, left: 10, bottom: 10}}
-        data={data}
-        barCategoryGap={4}
-        >
-       <XAxis dataKey='month_year' type='category' tickCount={14}/>
-       <YAxis/>
-       <CartesianGrid strokeDasharray="3 3"/>
-       <Tooltip/>
-       <Legend />
-       <ReferenceLine y={0} stroke='#000'/>
-         {fut ? <Bar dataKey='last' fill='black' /> : null}
-         {prof ?
-         <Bar dataKey='profit' >
-           {
-             data.map((month, index) => (
-             <Cell fill={(month.profit > month.minMargin) ? '#4dffa6' : '#404040'} />
-           ))
-         }
-        </Bar> : null}
-         {marg ? <Bar dataKey='margProfit' fill='#D3D3D3' /> : null}
-     </BarChart>
-     </div>
-    );
-  }
-}
-  export default ReChart;
+        <ResponsiveContainer width='100%' height='100%' aspect={1.6}>
+          <BarChart
+            margin={{top: 10, right: 10, left: 10, bottom: 10}}
+            data={data}
+            barCategoryGap={4}
+            >
+              <XAxis dataKey='month_year' type='category' tickCount={14} interval={0}/>
+              <YAxis unit='$' type='number'/>
+              <CartesianGrid strokeDasharray="3 3"/>
+              <Tooltip separator=': $'/>
+              <Legend />
+              <ReferenceLine y={0} stroke='#000'/>
+              {fut ? <Bar dataKey='last' fill='black' /> : null}
+              {prof ?
+                <Bar dataKey='profit' >
+                  {
+                    data.map((month, index) => (
+                      <Cell fill={(month.profit > month.minMargin) ? '#4dffa6' : '#404040'} />
+                    ))
+                  }
+                </Bar> : null}
+                {marg ? <Bar dataKey='marginal_profit' fill='#298378' /> : null}
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        );
+      }
+    }
+    export default ReChart;
